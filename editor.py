b69e0db8d95089d229ebd6f09fa2b24de8ce2d12
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-01 14:02:42 -0500

    Fix image rotation in editing script to rotate images by -90 degrees

--- editor.py
+++ editor.py
@@ -7,7 +7,7 @@ pathOut = '/editedImages'
 for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
 
-    edit = img.filter(ImageFilter.SHARPEN).convert('L') # sharpens image and rotates it
+    edit = img.filter(ImageFilter.SHARPEN).rotate(-90)
 
     clean_name = os.path.splitext(filename)[0] # isolates name of file without extension
 
