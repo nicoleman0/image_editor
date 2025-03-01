d58964538efc0fcfb03adecc88c99f71a05ea88b
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-01 14:08:56 -0500

    Enhance image editing script to include contrast adjustment after sharpening and rotation

--- editor.py
+++ editor.py
@@ -6,9 +6,16 @@ pathOut = '/editedImages'
 
 for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
+    
+    # sharpens image and rotates by 90 degrees
+    edit = img.filter(ImageFilter.SHARPEN).rotate(-90) 
+    
+    # contrast enhance
+    contrast_factor = 2
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(contrast_factor)
 
-    edit = img.filter(ImageFilter.SHARPEN).rotate(-90)
-
-    clean_name = os.path.splitext(filename)[0] # isolates name of file without extension
+    # isolates name of file without extension
+    clean_name = os.path.splitext(filename)[0] 
 
     edit.save(f".{pathOut}/{clean_name}_edited.jpg")
